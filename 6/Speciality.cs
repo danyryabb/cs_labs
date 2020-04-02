ea4d43578117badd9ff9dc082d8d71c62414fcf0
Дэнчик <61152705+danyryabb@users.noreply.github.com>
2020-04-02 11:22:15 +0300

    Create Speciality.cs

---
+++ 6/Speciality.cs
@@ -0,0 +1,24 @@
+  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+   abstract class Spec : Human
+    {
+        public String Speciality { get; set; }
+
+        public Spec() : base()
+        {
+            Speciality = "Informatics and technologies of programming";
+        }
+
+        public Spec(string name, string country, int yearofbirth, string speciality) : base(name, country, yearofbirth)
+        {
+            Speciality = speciality;
+        }
+    }
+}    
