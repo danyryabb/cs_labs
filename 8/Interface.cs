cd02db631555c9382a8d601a0e5d7dfd14164b5a
Дэнчик <61152705+danyryabb@users.noreply.github.com>
2020-04-03 02:39:56 +0300

    Create Interface.cs

---
+++ 8/Interface.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addfuncs
+{
+    interface IMyInterface
+    {
+        string GetOlimpiad();
+    }
+    public interface IComparable
+    {
+        int CompareTo(object obj);
+    }
+} 
