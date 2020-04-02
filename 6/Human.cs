dc2b95a9c774aa64bde7adcbd986573947df6f5b
Дэнчик <61152705+danyryabb@users.noreply.github.com>
2020-04-02 11:11:41 +0300

    Create Human.cs

---
+++ 6/Human.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    abstract class Human : IMyInterfaceFirst
+    {
+        public String Name { get; set; }
+        public String Country { get; set; }
+        public int YearOfBirth { get; set; }
+        protected static int counter { get; set; }
+
+        public Human()
+        {
+            Name = "Dany Ryaby";
+            Country = "Belarus";
+            YearOfBirth = 2002;
+            counter++;
+        }
+        public Human(string name, string country, int yearofbirth)
+        {
+            Name = name;
+            Country = country;
+            YearOfBirth = yearofbirth;
+            counter++;
+        }
+        ~Human()
+        {
+            Name = null;
+            YearOfBirth = 0;
+            Country = null;
+            counter = 0;
+        }
+        public override string ToString() => $"Surame: {Name}\tYear of birth: {YearOfBirth}\t" +
+                $"Country: {Country}\tUnique id of object Human is  {counter}";
+        }
+    }
+}
